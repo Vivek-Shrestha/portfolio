4ac0f517acdef075dff3f967d9f53a7b32095706
Bibek Shrestha <bibek@example.com>
2024-01-11 23:38:37 +0545

    Card design in Project

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HeroImg2 from '../components/HeroImg2'
+import PricingCard from '../components/PricingCard';
+import WorkCard from '../components/WorkCard';
 
 const Project = () => {
   return (
@@ -9,6 +11,8 @@ const Project = () => {
       <Navbar/>
       <HeroImg2 heading="PROJECTS."
        text="Some of my most recent works"/>
+       <WorkCard/>
+       <PricingCard/>
       <Footer/>
 
     </div>
